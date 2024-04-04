f5ceac44395ee66e11cf98046596a6d8e747f9df
tinazhen08 <tinaz43@nycstudents.net>
2024-04-04 13:35:14 -0400

    Update monster_info.py

--- monster_info.py
+++ monster_info.py
@@ -1,5 +1,14 @@
 import json
 import os
 
+test = open("monster_data.json", encoding="utf8")
+monster_data = json.load(test)
+
 class monster_info:
-    
+    def monster():
+        name = input("Which monster would you like to know about?\n").upper()
+        for i in monster_data:
+            if name == i['name'].upper():
+                print(i)
+
+monster_info.monster()
