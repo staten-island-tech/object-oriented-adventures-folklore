3b4b604e070b5b32578b55f1ba03a5b8fbba37ff
tinazhen08 <tinaz43@nycstudents.net>
2024-04-08 13:47:48 -0400

    Update monster_info.py

--- monster_info.py
+++ monster_info.py
@@ -13,8 +13,34 @@ class monster_info:
                 print(i['Description'])
                 print(i['Loot'])
 
-class Maldachaunians:
-    def attack():
+class monsters:
+    def maldachaunians():
         hp = 100
         print("OOGAH BOOGAH!")
-        
+    def wimitescu():
+        hp = 200
+    def elves():
+        hp = 100
+    def water_spirits():
+        hp = 150 
+
+class encounter:
+    def enco_monster():
+        while True:
+            choose = input("What would you like to do? A/B/C\n(Type: 'help' for explanation on encounters)\n").upper()
+            while choose == "HELP":
+                print("A - Attack")
+                print("B - Be a wuss (run away)")
+                print("C - Consume potion")
+                break
+            if choose == "A":
+                print("You have attacked!")
+                break
+            elif choose == "B":
+                print("You are a huge wuss! Congrats on running away, YOU COWARD!")
+                break
+            elif choose == "C":
+                print("Which potion would you like to drink\n")
+                break
+
+encounter.enco_monster()
