9dd31ffe0a06ac156df72975648fc0449cfcd121
tinazhen08 <tinaz43@nycstudents.net>
2024-04-11 13:57:42 -0400

    Update life_system.py

--- life_system.py
+++ life_system.py
@@ -1,1 +1,11 @@
-full_hp = 100
+class hp:
+    def __init__(self,full_hp,hp_history):
+        self.full_hp = full_hp
+        self.hp_history = hp_history
+    
+    def __str__(self):
+        return f"{self.full_hp},{self.hp_history}"
+    
+    full_hp = 100
+    hp_history = []
+
