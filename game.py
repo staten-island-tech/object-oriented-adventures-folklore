5d3d6cabccf7a77ad752d7e91d19b9c7aa9441bf
tinazhen08 <tinaz43@nycstudents.net>
2024-04-12 13:58:19 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 import json
 import os
-
+from classes import start
 os.system('cls')
 print("Welcome to the Folklore Game!")
 name = input("What is your name?\n")
 os.system('cls')
 
+start.interface()
