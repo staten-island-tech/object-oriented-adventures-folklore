a610e2d40268322528a08ef8dac4f32c00888317
tinazhen08 <tinaz43@nycstudents.net>
2024-04-12 14:00:01 -0400

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,14 @@
+class start:
+    def interface():
+        print("1. Start Game")
+        print("2. How to play")
+        print("3. Exit Game")
+        pick_option = input("What would you like to do? (ex: 1, 2, or 3): \n")
+        os.system('cls')
+
+        if pick_option == "1":
+            print()
+        elif pick_option == "2":
+            print()
+        elif pick_option == "3":
+            print("Bye bye!")
