d97ec37175a1776ca31d587a96d5ecfbcb187265
daniellin-1989 <DAniell619@nycstudents.net>
2024-05-01 13:45:37 -0400

    no work

--- betty.py
+++ betty.py
@@ -52,76 +52,86 @@ class Global:
     def encountering_characters():
         typingPrint("\x1B[3m You decided to back to the road.")
         time.sleep(2)
-        os.system('clear')
+        os.system('cls')
         Betty.first_en_bet()
         ###work from here
 
 class Betty:
-    os.system('clear')
+    os.system('cls')
     def first_en_bet():
                 #After arriving in the village for the first time and you pick Betty as your friend.
         picking_friend()
         if picking_friends == "A":
                     #Betty's Lines
-            os.system('clear')
+            os.system('cls')
             typingPrint("\x1B[3m You find yourself drawn to the towering structure in the center of the city.\n Its spire reaches up to the sky, beckoning you to come closer.\n As you approach the base of the tower, you notice a young woman sitting on the steps.")
             time.sleep(5)
-            os.system('clear')
+            os.system('cls')
             
             def bet_Continue():
                 print("\x1B[0mStranger:")    
-                Climb_Poets_Tower = typingInput(boxed_msg("Are you here to climb the tortured tower? (yes/no)\n(You also have the option to ask for information about the tower.\n )")).upper()
+                Climb_Poets_Tower = typingInput(boxed_msg("Are you here to climb the Tortured Poets Department?\n (yes/no)\n(You also have the option to ask for information about the tower.)")).upper()
                 time.sleep(2)
-                os.system('clear')
+                os.system('cls')
 
                 if Climb_Poets_Tower == "YES":
-                    os.system('clear')
+                    os.system('cls')
                     print("Stranger")
                     typingPrint(boxed_msg("You better be careful. \nThe tower belonged to a powerful moonlit witch. \nNo one has ever made it to the top."))
                     time.sleep(5)
-                    os.system('clear')
+                    os.system('cls')
 
                     print("Travler:")
                     typingPrint(boxed_msg("Thank you for the advice."))
                     time.sleep(1)
-                    os.system('clear')
+                    os.system('cls')
 
-                    typingPrint("\x1B[3m You entered through the gates of the tower, knowing full well you have no experience.\n As you arrived to the stairs of the tower, a forced consumed you and you were transported back in time.")
+                    typingPrint("\x1B[3m You entered through the gates of the tower, knowing full well you have no experience.\n As you arrived to the stairs of the tower, a forced consumed you and you were transported back in time.\n You ended put back on the fork in the world. \nThe place your journey began.")
                     time.sleep(5)
-                    os.system('clear')
+                    os.system('cls')
+
+                    print("Travler:")
+                    typingPrint(boxed_msg("\x1B[0mWhat happened?"))
+                    time.sleep(1)
+                    os.system('cls')
+
 
                     Betty.first_en_bet()
 
 
                     
                 elif Climb_Poets_Tower == "NO":
-                    os.system('clear')
+                    os.system('cls')
                     not_climb_poet_tower()
                 else:
-                    os.system('clear')
+                    os.system('cls')
                     print("Betty:")
-                    typingPrint(boxed_msg("The tower is"))
+                    typingPrint(boxed_msg(Climb_Poets_Tower))
+
+                    os.system('cls')
+                    print("Betty:")
+                    typingPrint(boxed_msg("It was said that this tower belong to a powerful witch. \nHowever she wasn't the one who built this tower. \nLegends"))
 
                 def not_climb_poet_tower():
                     print("\x1B[0mTraveller:")
                     typingPrint(boxed_msg("No, not really.\n I am new to this town."))
                     time.sleep(5)
-                    os.system('clear')
+                    os.system('cls')
 
                     print("\x1B[0mBetty:")
                     typingPrint(boxed_msg("Hi, I am Betty and I can show you around.\nThis village is known for its fishing ports and advancements in the water way.\nAt the center of the village, we have a huge water fountain, which is like healing sprinkler.\nOnce you get near it, it should heal you completely."))
                     time.sleep(5)
-                    os.system('clear')
+                    os.system('cls')
 
                     print("\x1B[0mTraveller:")
                     typingPrint(boxed_msg("Thank you."))
                     time.sleep(5)
-                    os.system('clear')
+                    os.system('cls')
 
                     print("\x1B[0mBetty:")    
                     typingPrint(boxed_msg("Follow me, I can show you where you can get materials and supplies.\nThe merchant is very nice and the stuff is pretty cheap."))
                     time.sleep(5)
-                    os.system('clear')
+                    os.system('cls')
 
 
             going_to_Betty = typingInput("\x1B[3m Would you like to approach her? (yes/no)").upper()
