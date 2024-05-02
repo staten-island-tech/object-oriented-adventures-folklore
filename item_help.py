f981cd48bbdc4e061ada2ac4e90bed176723fabf
tinazhen08 <tinaz43@nycstudents.net>
2024-05-02 13:43:07 -0400

    Update item_help.py

--- item_help.py
+++ item_help.py
@@ -2,16 +2,6 @@ import json
 test = open("item_history.json", encoding = "utf8")
 data = json.load(test)
 
-class Item:
-    def Item_Description():
-        print("1. Start Game")
-        print_option = input("What would you like to do? \n")
-
-        if print_option == "1":
-            print()
-        else:
-            print_option == "2"
-
 class Items:
     def print():
         for i in data:
@@ -21,4 +11,5 @@ class Items:
         for i in data:
             if x in i['Item category']:
                 print(i["Item name"])
+
 Items.item_category()
