8aa1b425fa4a20b0f1cda7a23a15f5360461a03c
tinazhen08 <tinaz43@nycstudents.net>
2024-05-03 13:58:23 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,9 +5,5 @@ from classes import start
 from classes import monster_info
 from classes import encounter
 
-os.system('cls')
-print("Welcome to the Folklore Game!")
-name = input("What is your name?\n")
-
 start.interface()
 
