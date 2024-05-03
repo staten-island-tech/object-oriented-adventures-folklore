9608c787f073f6de7316061841e753d6b828884e
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-05-03 13:59:31 -0400

    6

--- environment.py
+++ environment.py
@@ -1,19 +1,32 @@
 import time
+import sys
+import os
 
-seconds = time.time()
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(typingspeed)
+   
+def typingInput(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(typingspeed)
+        value = input()  
+        return value  
 
-def print_seconds(hours, minutes, seconds):
-    print(3600*hours+60*minutes+seconds)
-
-
-print_seconds(1,2,3)
 def environment():
     while True:
-        player = input("ON/OFF): ").upper()
+        player = input("(ON/OFF): ").upper()
         if player == 'ON':
-            print("You have spawned in your last saved position. Press enter to continue with your journey.", seconds)
-            print("You are forest right now. Try obtaining wood and creating a weapon just in case you come across enemies", seconds)
+            typingPrint("You have spawned in your last saved position. Press enter to continue with your journey.")
+            time.sleep(4)
+            os.system('cls')
+            typingPrint("You are forest right now. Try obtaining wood and creating a weapon just in case you come across enemies")
+            time.sleep(4)
+            os.system('cls')
         else:
-            print("....",seconds)
+            print("....")
 environment()
         
