17467c6acd88b8d31a431c5f3c32e4112ed19372
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-05-04 16:23:28 +0000

    edit

--- monster.py
+++ monster.py
@@ -2,32 +2,29 @@ import json
 from classes import player
 
 class Monster:
-    def __init__(self, name, hp, description, loot):
+    def __init__(self, name: str, hp: int, description: str, loot: str):
         self.name = name
         self.hp = hp
         self.description = description
         self.loot = loot
-    def __str__(self):
-        return f"{self.name}, {self.hp}, {self.description}, {self.loot}"
     
 class maldachaunians(Monster):
-    def __init__(self, name, hp, description, loot, weapon):
+    def __init__(self, name:str, hp:int, description:str, loot:str, weapon:str):
         super().__init__(name, hp, description, loot)
         self.weapon = weapon
     def __str__(self):
         return f"{self.name}, {self.hp}, {self.description}, {self.loot}, {self.weapon}"
     
 class elves(maldachaunians):
-    def __init__(self, name, hp, description, loot, weapon, powers):
+    def __init__(self, name:str, hp:int, description:str, loot:str, weapon:str, powers:str):
         super().__init__(name, hp, description, loot, weapon)
         self.power = powers 
     def __str__(self):
         return f"{self.name}, {self.hp}, {self.description}, {self.loot}, {self.weapon}, {self.power}"
     
 class element_spirits(Monster): 
-    def __init__(self, name, hp, description, loot, power):
+    def __init__(self, name:str, hp:int, description:str, loot:str, power:str):
         super().__init__(name, hp, description, loot)
         self.power = power 
     def __str__(self):
         return f"{self.name}, {self.hp}, {self.description}, {self.loot}, {self.power}"
-    
