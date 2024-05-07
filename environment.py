d15f056b25356d222bb53a1285d33c5a7e006503
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-05-07 13:59:49 -0400

    4

--- environment.py
+++ environment.py
@@ -31,26 +31,42 @@ def environment():
             time.sleep(3)
             os.system('cls')
             typingPrint("Let's continue walking up ahead. There is the Maldachaunians Camps(left), lakes(foward), and the Wimitescu Swampland(right). Which direction would you like to go?")
-            time.sleep(4)
+            time.sleep(5)
             os.system('cls')
-            direction = input("LEFT/FOWARD/BACKWARD/RIGHT").upper()
-            if direction == "LEFT":
+            direction = input("Left/Foward/Backward/Right").upper()
+            if direction == "Left":
                 typingPrint("You have reached the Maldachaunian Camps. Once human beings who lived in the empire ruled by malevolent deities.")
                 time.sleep(3)
                 os.system('cls')
                 typingPrint("Under the tyrannical rule of these godly beings, humans were forced to work in underground mines. One of these mines was the Chasm of Carcassona. The people of this mine were directed to collect beryl crystals, terribly hard to acquire, with many casualties.")
-                time.sleep(4)
+                time.sleep(5)
                 os.system('cls')
                 typingPrint("A plot to escape was quickly formed. A false explosion created a perfect distraction for an escape. However, the plan failed and they were cursed to be made and violent creatures.")
                 time.sleep(4)
                 os.system('cls')
                 typingPrint("But nevermind that. This place is not used to earn more loot by defeating enemies. Think of it as a training ground for you to get stronger.")
-                time.sleep(3)
+                time.sleep(4)
                 os.system('cls')
-            elif direction == "FOWARD":
-                typingPrint("Yes")
+            elif direction == "Foward":
+                typingPrint("You have reached the lakes. If you walk in far enough you can reach either the village ports or water spirit Fjord. ")
+                time.sleep(4)
+                os.system('cls')
+                typingPrint("..*you walk for another 5 min*....")
+                time.sleep(2)
+                os.system('cls')
+                direction = input("Left/Right").upper()
+            elif direction == "Backward":
+                typingPrint("You choose to go backward. This means that you will go back through the forest and arrive somewhere else")
+                time.sleep(4)
+                os.system('cls')
+                direction = input("Left/Foward/Backward/Right").upper()
+                #right direction for else
             else:
-                typingPrint("Yes")
+                typingPrint("You have arrived at the Wimitescu Swampland. If you didn't know this is where sassy shrek lives. Try not to run into sassy shrek or things could get really bad for you.")
+                time.sleep(4)
+                os.system('cls')
+                typingPrint("")
+
         else:
             print("....")
 environment()
