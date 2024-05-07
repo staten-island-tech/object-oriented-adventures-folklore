24044543f10e23fd9a06b255229a3060e5e941f1
tinazhen08 <tinaz43@nycstudents.net>
2024-05-07 13:36:03 -0400

    Update player.py

--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 class player:
-    def __init__(self, name: str, hp: int, strength: int, speed: int, inventory: str):
+    def __init__(self, name: str, hp: int, strength: int, speed: int, inventory: str, damage: int):
         self.name = name
         self.hp = hp
         self.strength = strength
         self.speed = speed
         self.inventory = inventory 
+        self.damage = damage
 
     def __str__(self):
-        return f"{self.name}, {self.hp}, {self. strength}, {self.speed}, {self.inventory}"
+        return f"{self.name}, {self.hp}, {self. strength}, {self.speed}, {self.inventory}, {self.damage}"
+
