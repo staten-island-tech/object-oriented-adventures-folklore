6c16c7bcb0f9d88f83dc0e3e8d5cb431af5e6182
tinazhen08 <tinaz43@nycstudents.net>
2024-05-08 13:38:21 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -40,26 +40,6 @@ class boxes:
 test = open("monster_data.json", encoding = "utf8")
 monster_data = json.load(test)
 
-class start:
-    def interface():
-        print("1. Start Game")
-        print("2. How to play")
-        print("3. Exit Game")
-        pick_option = input("What would you like to do? (ex: 1, 2, or 3): \n")
-        os.system('cls')
-
-        if pick_option == "1":
-            print()
-            os.system('cls')
-
-        elif pick_option == "2":
-            print()
-            os.system('cls')
-
-        elif pick_option == "3":
-            print("Bye bye!")
-            os.system('cls')
-
 class monster_info:
     def search_name():
         name = input("Which monster would you like to know about?\n").upper()
