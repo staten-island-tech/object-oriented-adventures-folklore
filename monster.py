7abc92758e3dc270166c7b1de3785eff3bc91b11
tinazhen08 <tinaz43@nycstudents.net>
2024-05-10 13:54:44 -0400

    Update monster.py

--- monster.py
+++ monster.py
@@ -2,24 +2,24 @@ import json
 from classes import player
 
 class Monster:
-    def __init__(monster, name: str, hp: int, description: str, loot: str):
-        monster.name = name
-        monster.hp = hp
-        monster.description = description
-        monster.loot = loot
+    def __init__(self, name: str, hp: int, description: str, loot: str):
+        self.name = name
+        self.hp = hp
+        self.description = description
+        self.loot = loot
     
 class maldachaunians(Monster):
-    def __init__(monster, name:str, hp:int, description:str, loot:str, weapon:str):
+    def __init__(self, name:str, hp:int, description:str, loot:str, weapon:str):
         super().__init__(name, hp, description, loot)
-        monster.weapon = weapon
+        self.weapon = weapon
 
 class elves(maldachaunians):
-    def __init__(monster, name:str, hp:int, description:str, loot:str, weapon:str, powers:str):
+    def __init__(self, name:str, hp:int, description:str, loot:str, weapon:str, powers:str):
         super().__init__(name, hp, description, loot, weapon)
-        monster.power = powers 
+        self.power = powers 
     
 class element_spirits(Monster): 
-    def __init__(monster, name:str, hp:int, description:str, loot:str, power:str):
+    def __init__(self, name:str, hp:int, description:str, loot:str, power:str):
         super().__init__(name, hp, description, loot)
-        monster.power = power 
+        self.power = power 
 
