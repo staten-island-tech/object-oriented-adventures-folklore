e9d618c0f53f378947400219df21d59b736c4650
Philijoy <158211486+Philijoy@users.noreply.github.com>
2024-05-15 14:00:05 -0400

    5

--- item_history.py
+++ item_history.py
@@ -26,40 +26,4 @@ def data_collection():
 if __name__ == "__main__":
     data_collection()
 
-    if input == ("item.help"):
-        print (Item_description)
 
-Item_description = [{
-    'Item name': "Sword",
-    'Item cateogory': "Weapon/Combat",
-    'Description': "Used in combat: Strength is dependent on the sharpness and level of the sword ()",
-}, {
-    'Item name': "Armor",
-    'Item cateogory': "Combat",
-    'Description': "A layer of protection that increases the damage needed to be dealt to you in order to lose health",
-},{
-    'Item name': "Health Potion",
-    'Item cateogory': "Spell",
-    'Description': "By intiating this potion, you gain 2 HP for every second that passes (Note: this potion lasts 10 seconds)",
-},{
-    'Item name': "Speed Potion",
-    'Item cateogory': "Spell",
-    'Description': "By intiating this potion, you gain 2x speed for 6 seconds",
-},{
-    'Item name': "Apple",
-    'Item cateogory':"Food",
-    'Description': "Can be consumed. Found in the wild or can be obtained through tree planting",
-}{
-    'Item name': "Cake",
-    'Item cateogory':"Food",
-    'Description': "Can be consumed. Must be made through ingredients: flour, eggs, sugar, and milk",
-}{
-    'Item name': "Pickaxe",
-    'Item cateogory': "Weapon/Combat",
-    'Description': "Used in combat: Strength is dependent on the level of the pickaxe",
-}{
-    'Item name': "Bow",
-    'Item cateogory':"Weapon/Combat",
-    'Description': "Used in combat: Made with wood and string, and requires an arrow to be intiated",
-}]
-print(Item_description)
---
+++ merhcant.py
@@ -0,0 +1,25 @@
+import time
+import sys
+import os
+
+global typingPrint
+global typingspeed
+typingspeed = 0.01
+global typingInput
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(typingspeed)
+
+def typingInput(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(typingspeed)
+        value = input()  
+        return value  
+    
+class Merchant():
+    def trading():
+        typingPrint("")
