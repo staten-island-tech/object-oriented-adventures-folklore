267374f5312b84c896ef1fc468658d60b6e0d784
tinazhen08 <tinaz43@nycstudents.net>
2024-05-16 13:57:36 -0400

    Update player.py

--- player.py
+++ player.py
@@ -27,27 +27,40 @@ class player():
         c = a + b
         n = c
         return n
-
-    def attack_enemy(a,b,c):
-        #a is the monter's hp, b is how much damage the player does, c is the player's crit attack amount
-        d = random.randint(1,20)
-        if d < 20:
-            a - b
-            print("You did " + str(a) + " damage!")
-
-        elif d == 20:
-            a - c
-            print("You did " + str(c) + " damage!")
-        return a
     
+    global m 
+    def attack_enemy(a,b,c):
+            #a is the monter's hp, b is how much damage the player does, c is the player's crit attack amount
+            d = random.randint(1,20)
+            if d < 20:
+                m = a - b
+                print("You did " + str(b) + " damage! It only has " + str(m) + " hp now!")
+                while m > 0:
+                    m - b
+            elif d == 20:
+                m = a - c
+                print("Your attack was a critical hit! You did " + str(c) + " damage! It only has " + str(m) + " hp now!")
+                while m > 0:
+                    m - c
+            return m
+    
+    global y 
     def get_attacked(a,b,c):
         #a is player hp, b is how much damage the monster does, c is a critical hit
         x = random.randint(1,20)
         if x < 20:   
-            h = a - b
-            print("You were attacked! You took " + str(b) + " damage and now have " + str(h) + " hp!")
+            y = a - b
+            print("You were attacked! You took " + str(b) + " damage and now have " + str(y) + " hp!")
 
         elif x == 20:
-            h = a - c
-            print("You were attacked! It was a critical hit! You took " + str(c) + " and now have" + str(h) + " hp!")
-        return h
+            y = a - c
+            print("You were attacked! It was a critical hit! You took " + str(c) + " and now have" + str(y) + " hp!")
+        return y
+    
+    def battle(a):
+        if a == "BEGINNING ELF":
+            player.attack_enemy(50,5,10)
+
+player.battle("BEGINNING ELF")
+
+
