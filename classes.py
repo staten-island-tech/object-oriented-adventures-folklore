30715d89c0a2ca9cb15d6da146f82466e5692d33
tinazhen08 <tinaz43@nycstudents.net>
2024-05-22 13:52:09 -0400

    delete premature tower merge

--- classes.py
+++ classes.py
@@ -11,12 +11,14 @@ class typer():
         for character in text:
             sys.stdout.write(character)
             sys.stdout.flush()
-            time.sleep(0.01)
+            speed = 0.01
+            time.sleep(speed)
     def typingInput(text):
         for character in text:
             sys.stdout.write(character)
             sys.stdout.flush()
-            time.sleep(0.01)
+            speed = 0.01
+            time.sleep(speed)
         value = input()  
         return value  
     
