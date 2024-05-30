eff1ddeb59acf46debcd8cb55d37845904c60fe5
daniellin-1989 <DAniell619@nycstudents.net>
2024-05-30 13:52:49 -0400

    sxdsd

--- merhcant.py
+++ merhcant.py
@@ -22,41 +22,47 @@ def typingInput(text):
     
 class Merchant:
     Player.Players()
-    def __init__(self, name, items):
+    def __init__(self, name, potions, foods, materials):
         self.name = name
-        self.items = items
+        self.potions = potions
+        self.foods = foods
+        self.materials = materials
               
     def display_items(self):
         typingPrint("\nYou walk towards the lovely shop centered in the middle of a market.")
         time.sleep(1)
         os.system('cls')
-        typingPrint(f"Shop Clerk: Hail traveler, Welcome to {self.name}!")
+        print("Merchant:")
+        typingPrint(f"Welcome traveler to {self.name}!")
         time.sleep(1)
         os.system('cls')
-        print("Take a look at my wares. Let me know if anything catches your eye.")
+        print("Take a look at my stick. Let me know if anything catches your eye.")
         time.sleep(1)
         os.system('cls')
-        for item, price in self.items.items():
+        for item, price in self.potions.potions():
+            print(f"{item.capitalize()}: {price} gold")
+        for item, price in self.foods.foods():
+            print(f"{item.capitalize()}: {price} gold")
+        for item, price in self.materials.materials():
             print(f"{item.capitalize()}: {price} gold")
                 
     def sell_item(self, item, quantity, buyer):
-        if item not in self.items:
+        if item not in self.potions or self.foods or self.materials:
             print(f"Appologies traveler, we currently don't sell {item}s.")
             print("You exit the store and make your way back to town with a visible look of disappointment at the news they didnt have your requested item.")
-        elif self.items[item] * quantity > buyer.resources["gold"]:
+        elif (self.potions[item] or self.foods[item] or self.materials[item]) * quantity > buyer.currency["Gold"]:
             print("  ")
             print("The old man stares at you questioning if your serious with your offer.")
             print("  ")
             print("Old man: You insult my shop with your offer. Come again once you have enough gold to afford my wares.")
             print("  ")
             print("You exit his shop and make your way back to the town.")
-        else:
-            buyer.items[item] -= quantity
+        elif buyer.potions[item] -= quantity or buyer.materials[item] -= quantity or buyer.foods[item] -= quantity:
             #might change to self.currency
-            Player.Players.currency["gold"] -= self.items[item] * quantity
+            Player.Players.currency["Gold"] -= self.potions[item] * quantity
             buyer.inventory.add_item(item, quantity)
             print("  ")
-            print(f"Old man: Ahh, exelent choice. Here is your {item} that will be {self.items[item] * quantity} gold.")
+            print(f"Old man: Ahh, exelent choice. Here is your {item} that will be {self.potions[item] * quantity} gold.")
             print("  ")
             print(f"The old man holds one hand out expecting gold holding the {item} in the other. As you drop the gold into his hand he gives you the blood vial. You walk out the door and back towards the town, satisfied with your purchase.")
 
