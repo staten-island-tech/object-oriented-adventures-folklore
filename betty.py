76a276232d0145fa5255bd8370cdb54393814f9d
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-06-08 17:27:25 -0400

    fix npc dialogue

--- betty.py
+++ betty.py
@@ -21,35 +21,37 @@ class Betty:
                 Yes_Going_to_Betty()
             else:
                 typingPrint("Are you stupid?")
+                time.sleep(2)
+                os.system("cls")
                 Choosing_Going_to_Betty()
         global Yes_Going_to_Betty
         def Yes_Going_to_Betty():
             os.system('cls')
             print("\x1B[0mStranger:")    
             global Climb_Poets_Tower
-            Climb_Poets_Tower = typingInput(boxed_msg("Either way, Betty approaches you. Are you here to climb the Tortured Poets Department?\n (yes/no)\n(You also have the option to ask for information about the tower.)")).upper()
+            Climb_Poets_Tower = typingInput(boxed_msg("Either way, Betty approaches you. Are you here to climb the Tortured Poets Department? Y/N \n(You also have the option to ask for information about the tower.)")).upper()
             time.sleep(2)
             os.system('cls')
 
         
         global Input_Climb_Poets_Tower
         def Input_Climb_Poets_Tower():
-            if Climb_Poets_Tower == "YES":
+            if Climb_Poets_Tower == "Y":
                 os.system('cls')
                 yes_to_climb_poets_tower()
-            if Climb_Poets_Tower == "NO":
+            if Climb_Poets_Tower == "N":
                 os.system('cls')
                 not_climb_poet_tower()
                 
 
         def yes_to_climb_poets_tower():
             os.system('cls')
-            print("Stranger")
+            print("\x1B[0mStranger")
             typingPrint(boxed_msg("You better be careful. \nThe tower belonged to a powerful moonlit witch. \nNo one has ever made it to the top."))
             time.sleep(5)
             os.system('cls')
 
-            print("Travler:")
+            print("\x1B[0mYou:")
             typingPrint(boxed_msg("Thank you for the advice."))
             time.sleep(1)
 
@@ -58,14 +60,14 @@ class Betty:
             time.sleep(5)
             os.system('cls')
 
-            print("Travler:")
-            typingPrint(boxed_msg("\x1B[0mWhat happened?"))
+            print("\x1B[0mYou:")
+            typingPrint(boxed_msg("What happened?"))
             time.sleep(1)
             os.system('cls')
             Choosing_Going_to_Betty()
             
         def not_climb_poet_tower():
-            print("\x1B[0mTraveller:")
+            print("\x1B[0mYou:")
             typingPrint(boxed_msg("No, not really.\n I am new to this town."))
             time.sleep(5)
             os.system('cls')
@@ -75,7 +77,7 @@ class Betty:
             time.sleep(5)
             os.system('cls')
 
-            print("\x1B[0mTraveller:")
+            print("\x1B[0mYou:")
             typingPrint(boxed_msg("Thank you."))
             time.sleep(5)
             os.system('cls')
--- james.py
+++ james.py
@@ -25,7 +25,7 @@ class James:
             time.sleep(3)
             os.system('cls')
 
-            print("\x1B[0mTraveller:")
+            print("\x1B[0mYou:")
             typingPrint(boxed_msg("..."))
             time.sleep(1)
             os.system('cls')
@@ -35,7 +35,7 @@ class James:
             time.sleep(2)
             os.system('cls')
 
-            print("\x1B[0mTraveller:")
+            print("\x1B[0mYou:")
             typingPrint(boxed_msg("I am new to this town."))
             time.sleep(2)
             os.system('cls')
@@ -45,45 +45,53 @@ class James:
             time.sleep(3)
             os.system('cls')
 
-            print("\x1B[0mTraveller:")
+            print("\x1B[0mYou:")
             typingPrint(boxed_msg("I guess I would join you."))
             time.sleep(2)
             os.system('cls')
 
             print("\x1B[0mStranger:")
             typingPrint(boxed_msg("Okay great. \nI will show you around. \nMy names is James by the way. \nThis is the underground alley of this town.\nThis is basically the black market."))
-            time.sleep(3)
+            time.sleep(5)
             os.system('cls')
 
             typingPrint("\x1B[3mYou have the option to punch him, while he is distracted or just wait and see what happends...")
-            PunchJames = typingInput("Would you like to punch games? (yes/no)").upper()
-            if PunchJames == "YES":
+            PunchJames = typingInput("Would you like to punch james? Y/N\n").upper()
+            if PunchJames == "Y":
                 print("\x1B[0mJames:")
                 typingPrint(boxed_msg("You back stabbing mole rat!"))
                 time.sleep(3)
                 os.system('cls')
 
-                PunchingJames = typingInput("Run or Slaughter him? (run/slaughter)").upper()
+                PunchingJames = typingInput("Run or Slaughter him? RUN/SLAUGHTER ").upper()
                 if PunchingJames == "RUN":
-                    typingPrint("\x1B[0mYou have escaped and you reached the merchant.")
+                    typingPrint("\x1B[3mYou have escaped and you reached the merchant.")
+                    time.sleep(2)
+                    os.system('cls')
+                    merhcant.Merchant()
                 if PunchingJames == "SLAUGHTER":
-                    typingPrint("\x1B[0mYou murdered James, and you run to the river to clean yourself up. Then you ran towards the mercahnt to get other materials.")
+                    typingPrint("\x1B[3mYou murdered James, and you run to the river to clean yourself up.\nThen you ran towards the mercahnt to get other materials.")
+                    time.sleep(3)
                     os.system('cls')
                     merhcant.Merchant()
 
-            if PunchJames == "NO":
+            if PunchJames == "N":
                 print("\x1B[0mJames:")
                 typingPrint(boxed_msg("You should never trust a me."))
                 time.sleep(3)
                 os.system('cls')
 
                 typingPrint("\x1B[3mJames murdered you.")
+                time.sleep(1)
                 os.system('cls')
-                typingPrint("You slowly lose your vision.")
+                typingPrint("\x1B[3mYou slowly lose your vision.")
+                time.sleep(1)
                 os.system('cls')
-                typingPrint("A clock appeared in front of your eyes as it started ticking backwards.")
+                typingPrint("\x1B[3mA clock appeared in front of your eyes as it started ticking backwards.")
+                time.sleep(3)
                 os.system('cls')
-                typingPrint("A bright light consumed you, sending you back in time.")
+                typingPrint("\x1B[3mA bright light consumed you, sending you back in time.")
+                time.sleep(2)
                 os.system('cls')
                 start.run_interface()
 
