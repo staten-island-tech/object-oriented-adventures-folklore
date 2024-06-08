80c4501b84e8b5861da5f3044737bfa933827ff6
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-06-08 17:03:57 -0400

    small changes

--- NPC.py
+++ NPC.py
@@ -4,7 +4,6 @@ from chu import first_en_Chu
 from james import first_en_Jame
 
 class picking_NPC:
-    global picking_friend
     def picking_friend():
         picking_friends = typingInput("\x1B[3mWould you like to move B(left), C(right), or J(straight ahead)?\n").upper()
         if picking_friends == "B":
@@ -14,7 +13,4 @@ class picking_NPC:
         elif picking_friends == "J":
             first_en_Jame()
 
-    def run_NPC():
-        picking_friend()
-
-picking_NPC.run_NPC()
+picking_NPC.picking_friend()
